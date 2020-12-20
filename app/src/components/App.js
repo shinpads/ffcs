55024999b96335d72b090ea97c7459ad92e56307
Rob <robfarlow@gmail.com>
2020-12-20 15:14:56 -0500

    remove wrapper

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -8,12 +8,10 @@ import Home from './pages/Home';
 class App extends Component {
   render() {
     return (
-      <>
-        <Switch>
-          <Route path="/" component={Home} exact />
-          <Redirect from="*" to="/" />
-        </Switch>
-      </>
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Redirect from="*" to="/" />
+      </Switch>
     );
   }
 }
