76713b9b45a0f238ea3ee7a02423ea0139a5fac3
Rob <robfarlow@gmail.com>
2020-12-20 15:27:09 -0500

    Setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  parser: "babel-eslint",
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    ga: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/jsx-filename-extension': 'off',
+    'react/forbid-prop-types': 'off',
+    'react/require-default-props': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'no-plusplus': 'off',
+    'arrow-parens': 'off',
+    'no-console': 'off',
+    'max-len': 'off',
+    'no-use-before-define': 'off',
+    'prefer-destructuring': 'warn',
+    'react/no-string-refs': 'warn',
+    'jsx-a11y/click-events-have-key-events': 'warn',
+    'jsx-a11y/no-noninteractive-element-interactions': 'warn',
+    'no-underscore-dangle': 'off',
+    'no-await-in-loop': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'jsx-a11y/label-has-associated-control': 'off',
+    'jsx-a11y/no-static-element-interactions': 'off',
+  },
+};
