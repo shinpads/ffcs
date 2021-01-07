db74e989f71e0e1f77c24fafeeb5dbb7b4aeaf06
andrewvii <andrewgall12@gmail.com>
2021-01-07 02:54:12 -0500

    backend implementation of riot tourney api

--- app/utils.py
+++ app/utils.py
@@ -9,12 +9,12 @@ def get_riot_account_id(username):
     url = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/"
     url = url + username + "?api_key=" + os.getenv('RIOT_API_KEY')
 
-    res = json.loads(requests.get(url))
+    res = requests.get(url)
     if res.status_code != 200:
         return None
 
-    body = res.text
-    account_id = body['accountId']
+    body = json.loads(res.text)
+    account_id = body['id']
 
     return str(account_id)
 
@@ -37,7 +37,6 @@ def register_tournament_provider():
     return str(res.text)
 
 def register_tournament(name, provider_id):
-    print(provider_id)
     debug = os.getenv('DEBUG')
     if debug == "on":
         url = "https://americas.api.riotgames.com/lol/tournament-stub/v4/tournaments"
@@ -50,8 +49,60 @@ def register_tournament(name, provider_id):
     }
 
     res = (requests.post(url, data=json.dumps(data)))
-    print(res.text)
     if res.status_code != 200:
+        print(res.text)
         return None
     
     return str(res.text)
+
+def generate_tournament_code(game, all_players):
+    summoners = []
+    map_type = "SUMMONERS_RIFT"
+    metadata = {}
+    pick_type = "TOURNAMENT_DRAFT"
+    spectator_type = "ALL"
+    team_size = 5
+
+    debug = os.getenv('DEBUG')
+    if debug == "on":
+        url = "https://americas.api.riotgames.com/lol/tournament-stub/v4/codes"
+    else:
+        url = "https://americas.api.riotgames.com/lol/tournament/v4/codes"
+    url = url + "?count=1"
+    url = url + "&tournamentId=" + game.match.season.tournament_id
+    url = url + "&api_key=" + os.getenv('RIOT_API_KEY')
+
+    if game.meta_key == '':
+        return None
+    else:
+        metadata["key"] = game.meta_key
+    
+    for team in game.match.teams.all():
+        for player in team.player_set.all():
+            summoners.append(player.account_id)
+    
+    if len(summoners) < 10:
+        return None
+    
+    for player in all_players.objects.filter(caster=True):
+        if player.account_id not in summoners:
+            summoners.append(player.account_id)
+    
+    data = {
+        "allowedSummonerIds": summoners,
+        "mapType": map_type,
+        "metadata": str(metadata),
+        "pickType": pick_type,
+        "spectatorType": spectator_type,
+        "teamSize": team_size
+    }
+
+    res = (requests.post(url, data=json.dumps(data)))
+
+    if res.status_code != 200:
+        print(res.text)
+        return None
+
+    code = json.loads(str(res.text))[0]
+
+    return code
--- app/views/gameviews.py
+++ app/views/gameviews.py
@@ -1,5 +1,7 @@
 from django.http import JsonResponse
 from django.views import View
+from ..models import Game, Player
+from ..utils import get_riot_account_id
 import json
 
 class CallbackView(View):
@@ -7,6 +9,48 @@ class CallbackView(View):
     def post(self, request, *args, **kwargs):
         data = json.loads(request.body)
         out_data = {}
+
+        game_id = data["gameId"]
+        meta_key = json.loads(data["metaData"])["key"]
+        
+        game = Game.objects.filter(meta_key=meta_key).first()
+        if game == None:
+            response = JsonResponse({
+                "message": "Could not find game.",
+                "data": out_data,
+            }, status=500)
+            return response
+
+        winner_acc_username = data["winningTeam"][0]["summonerName"]
+        winner_acc_id = get_riot_account_id(winner_acc_username)
+        winner_player = Player.objects.filter(account_id=winner_acc_id).first()
+        if winner_player == None:
+            response = JsonResponse({
+                "message": "Error finding player.",
+                "data": out_data,
+            }, status=500)
+            return response
+        
+        winner_team = winner_player.team
+        if winner_team == None:
+            response = JsonResponse({
+                "message": "Player does not have a team.",
+                "data": out_data,
+            }, status=500)
+            return response
+
+        if game.winner != None or game.game_id != '':
+            response = JsonResponse({
+                "message": "That game has already been updated.",
+                "data": out_data,
+            }, status=500)
+            return response
+
+        game.winner = winner_team
+        game.game_id = game_id
+        game.save()
+        
+
         response = JsonResponse({
             "message": "Successfully recieved callback.",
             "data": out_data,
