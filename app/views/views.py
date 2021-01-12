1e3cf88b04a9fb0debd57750e5997fc393a9ce44
andrewvii <andrewgall12@gmail.com>
2021-01-11 22:14:39 -0500

    added riot.txt

--- app/views/views.py
+++ app/views/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def index(request):
   return render(request, 'index.html')
+
+def riot(request):
+  filename = "riot.txt"
+  content = 'aewrhbaw'
+  response = HttpResponse(content, content_type='text/plain')
+  response['Content-Disposition'] = 'attachment; filename={0}'.format(filename)
+  return response
