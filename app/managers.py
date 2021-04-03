70661339cb1f59127f84d1cf39a416d0d5a85ea5
Rob Farlow <robfarlow@gmail.com>
2021-04-02 20:14:02 -0400

    Update managers.py

--- app/managers.py
+++ app/managers.py
@@ -2,9 +2,12 @@ from django.contrib.auth import models
 
 class DiscordUserOAuthManager(models.UserManager):
     def create_new_discord_user(self, user):
+        avatar = user['avatar']
+        if avatar == None:
+            avatar = ''
         return self.create(
             discord_user_id=user['id'],
             email=user['email'],
-            avatar=user['avatar'],
+            avatar=avatar,
             discord_username='%s#%s' % (user['username'], user['discriminator'])
         )
