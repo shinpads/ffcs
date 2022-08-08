14d1acf6e0afc057d3f11cad4fecaa7bc3111d1a
Andrew <andrewgall12@gmail.com>
2022-08-07 21:54:18 -0400

    Schedule fix

--- app/apps.py
+++ app/apps.py
@@ -1,9 +1,14 @@
 from django.apps import AppConfig
-
+import os
 
 class AppConfig(AppConfig):
     name = 'app'
 
     def ready(self):
+        run_once = os.environ.get('SCHED_RUN_ONCE')
+        if run_once is not None:
+            return
+        os.environ['SCHED_RUN_ONCE'] = 'True'
+        
         from . import scheduler
         scheduler.start()
